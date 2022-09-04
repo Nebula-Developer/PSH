54894ea4fb680f2ea792d0b292fdba9349a83141
Nico Cook <nebuladev.contact@gmail.com>
2022-09-04 19:55:55 +1200

    Cleanup and deps
    Added files which will be filled later (and general cleanup)

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
 namespace PSH {
     public class PearlShell {
         public static void Main(string[] args) {
-            Dictionary<String, dynamic> config = ConfigHandling.Config.ReadConfig("/Volumes/PSH/config.conf", true) ?? new Dictionary<String, dynamic>();
+            
         }
     }
 }
