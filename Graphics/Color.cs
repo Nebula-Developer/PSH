051affc9e34b9f12c648793247f014093fb494e8
Nico Cook <nebuladev.contact@gmail.com>
2022-09-07 10:48:26 +1200

    Improved maths and graphics class

--- Graphics/Color.cs
+++ Graphics/Color.cs
@@ -17,5 +17,25 @@ namespace PSH.Graphics {
         public static string CreateEsc(int r, int g, int b) {
             return $"\x1b[38;2;{r};{g};{b}m";
         } 
+
+        public string ToHexStr() {
+            return $"#{this.R:X2}{this.G:X2}{this.B:X2}";
+        }
+    }
+
+    public class Hex {
+        public string HexStr;
+
+        public Hex(string hex) {
+            this.HexStr = hex;
+        }
+
+        public RGB ToRGB() {
+            return new RGB(
+                Convert.ToInt32(this.HexStr.Substring(1, 2), 16),
+                Convert.ToInt32(this.HexStr.Substring(3, 2), 16),
+                Convert.ToInt32(this.HexStr.Substring(5, 2), 16)
+            );
+        }
     }
 }
